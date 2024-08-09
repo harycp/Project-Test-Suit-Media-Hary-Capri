7ea31aefaf0eba5612904ccd1232191165c9510b
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-08-09 23:17:02 +0700

    transparant

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -73,7 +73,7 @@ export default {
   width: 100%;
   z-index: 100;
   top: 0;
-  background-color: rgba(240, 108, 52, 0.9);
+  background-color: rgba(240, 108, 52, 0.7);
   color: white;
   left: 0;
 }
